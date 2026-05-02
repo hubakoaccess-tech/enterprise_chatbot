c390e86f4f7143d1411311e5605b18d1d306808f
Vidur Agarwal <viduragarwal22@gmail.com>
2026-05-02 20:32:30 +0530

    Fix numpy version and staticfiles

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+mkdir -p staticfiles
 python manage.py collectstatic --no-input
 python manage.py migrate
